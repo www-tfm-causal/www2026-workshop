b4b515db53727f246308aeeea5c8b5464a395286
Xun_ASUS25 <597774168@qq.com>
2025-12-05 20:01:50 +0800

    Init for all files

---
+++ js/script.js
@@ -0,0 +1,85 @@
+/*
+Theme: Flatfy Theme
+Author: Andrea Galanti
+Bootstrap Version 
+Build: 1.0
+http://www.andreagalanti.it
+*/
+
+// 创建页面加载和超时的Promise
+var pageLoad = new Promise(function(resolve) {
+    $(window).on('load', resolve);
+});
+
+var timeout = new Promise(function(resolve) {
+    setTimeout(resolve, 6000);
+});
+
+// 哪个先完成就隐藏预加载器
+Promise.race([pageLoad, timeout]).then(function() {
+    $('#status').delay(300).fadeOut();
+    $('#preloader').delay(300).fadeOut('slow');
+    $('body').delay(550).css({'overflow':'visible'});
+});
+
+$(document).ready(function() {
+		//animated logo
+		$(".navbar-brand").hover(function () {
+			$(this).toggleClass("animated shake");
+		});
+		
+		//animated scroll_arrow
+		$(".img_scroll").hover(function () {
+			$(this).toggleClass("animated infinite bounce");
+		});
+		
+		//Wow Animation DISABLE FOR ANIMATION MOBILE/TABLET
+		wow = new WOW(
+		{
+			mobile: false
+		});
+		wow.init();
+		
+		//MagnificPopup
+		$('.image-link').magnificPopup({type:'image'});
+
+
+		// OwlCarousel N1
+		$("#owl-demo").owlCarousel({
+			autoPlay: 3000,
+			items : 3,
+			itemsDesktop : [1199,3],
+			itemsDesktopSmall : [979,3]
+		});
+
+		// OwlCarousel N2
+		$("#owl-demo-1").owlCarousel({
+			  navigation : false, // Show next and prev buttons
+			  slideSpeed : 300,
+			  paginationSpeed : 400,
+			  singleItem:true
+		});
+
+		//SmothScroll
+		$('a[href*=#]').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+			&& location.hostname == this.hostname) {
+					var $target = $(this.hash);
+					$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+					if ($target.length) {
+							var targetOffset = $target.offset().top;
+							$('html,body').animate({scrollTop: targetOffset}, 600);
+							return false;
+					}
+			}
+		});
+		
+		//Subscribe
+		new UIMorphingButton( document.querySelector( '.morph-button' ) );
+		// for demo purposes only
+		[].slice.call( document.querySelectorAll( 'form button' ) ).forEach( function( bttn ) { 
+			bttn.addEventListener( 'click', function( ev ) { ev.preventDefault(); } );
+		} );
+
+});
+
